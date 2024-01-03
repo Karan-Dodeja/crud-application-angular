f35caede312971e5165bf84255a256f2c5c4edae
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-03 10:33:49 +0530

    View Post

--- src/app/post/view/view.component.ts
+++ src/app/post/view/view.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view.component.html',
-  styleUrl: './view.component.css'
+  styleUrl: './view.component.css',
 })
 export class ViewComponent {
+  id!: number;
+  post!: Post;
 
+  constructor(
+    public postSerview: PostService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+    this.postSerview.find(this.id).subscribe((data:any) => {
+      this.post = data;
+    })
+  }
 }
