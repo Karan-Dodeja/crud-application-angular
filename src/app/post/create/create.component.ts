e148d35070bed5d7aea6070d56938cf243b09fc6
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-03 10:17:11 +0530

    create form design

--- src/app/post/create/create.component.ts
+++ src/app/post/create/create.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PostService } from '../post.service';
+import { Router } from 'express';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
 })
 export class CreateComponent {
+  form!: FormGroup;
+  constructor(public postService: PostService, private router: Router, private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', Validators.required),
+    });
+  }
+
+  get() {
+    return this.form.controls;
+  }
+
+  submit(){
+    this.postService.create(this.form.value).subscribe((res:any) => {
+      alert("Post Created Successfully.");
+      (<any>this.router).navigate('post/index');
+    })
+  }
 }
